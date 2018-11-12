809a8467d9e408c55fc9c764f4a6a625e127870b
Sung Min Yoon <sungminyoon@sungs-macbook-air.local>
2018-11-11 23:30:47 -0500

    Initial Commit

---
+++ iVendSales/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iVendSales
+//
+//  Created by Sung Min Yoon on 2018-11-11.
+//  Copyright © 2018 Sung Min Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
